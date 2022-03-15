b21cf15ee8ebeeea4a5f891fb0ccebb2f824407b
Matt Hess <matthijn_hesselman@hotmail.com>
2022-03-15 11:02:31 +0100

    first commit

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // User::factory()->create();
+        factory(App\User::class)->create(['is_admin'=>true,'name' => 'matthijn', 'email' => 'matthijn@gmail.com', 'password' => bcrypt('password')]);
+        factory(App\User::class)->create(['is_admin'=>false,'name' => 'hendrik', 'email' => 'hendrik@gmail.com', 'password' => bcrypt('password')]);
+
+
+
+
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// Route::get('clientdata', 'ClientController@index');
+// // Route::post('deleteClientData', 'ClientController@delete');
+// Route::post('deleteClientData/{id}', 'ClientController@delete');
+// Route::post('createclient', 'ClientController@store');
+// Route::put('/client/{id}','ClientController@update');
+
+
+// Route::post('/upload', 'FileController@upload');
+
+
+// Route::apiResource('product', 'Api\ProductController');
+
+// // Verb          Path                        Action  Route Name
+// // GET           /users                      index   users.index
+// // POST          /users                      store   users.store
+// // GET           /users/{user}               show    users.show
+// // PUT|PATCH     /users/{user}               update  users.update
+// // DELETE        /users/{user}               destroy users.destroy
+
+
